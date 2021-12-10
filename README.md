f5d3d9446b1dc11710b1652d61204ef768b48f85
manuher16 <73207879+manuher16@users.noreply.github.com>
2021-12-09 20:55:03 -0600

    agregadro screen ha markdown

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # music_player_react
- Reproductor musica con React
+
+Reproductor musica con React
+Planilla de una vista de un reproductor, realizada con react,Css y iconos de bootstrap
+
+##Screenshots
+![scrrenshot 1](./assets/images/screenshot_1.jpg)
--- frontend/src/views/Player.js
+++ frontend/src/views/Player.js
@@ -1,4 +1,6 @@
 import "../styles/player.css";
+import React from 'react';
+import { useState, useEffect } from "react";
 const Player = (props) => {
     const styleControl1 = {
         marginTop: '40px'
@@ -6,21 +8,39 @@ const Player = (props) => {
     const styleControl2 = {
         marginBottom: '40px'
     }
-
-    const play = () => {
+    const [play, setPlay] = useState(false);
+    const [timeSong, setTimeSong] = useState(10);
+    const [timeLenghtSong, setTimeLenghtSong] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+    useEffect(() => {
         const audio = document.getElementById("audio");
-        const playPause = document.getElementById("play");
+        setTimeSong(audio.currentTime)
+        setTimeLenghtSong(audio.duration)
+    }, [])
+    const playMusic = () => {
+        const audio = document.getElementById("audio");
+        console.log("play");
+        const time_song = document.getElementById("time_song");
         if (audio.paused || audio.ended) {
-            playPause.querySelector(".pause-btn").classList.remove("hidden");
-            playPause.querySelector(".play-btn").classList.add("hidden");
+            console.log(audio.duration);
+            setInterval(() => {
+                setTimeSong(audio.currentTime);
+                console.log(time_song.value);
+                console.log(time_song.max);
+                time_song.value = audio.currentTime
+                time_song.max = audio.duration
+
+            }, 1000);
+            audio.play();
+
         } else {
             audio.pause();
-            playPause.querySelector(".pause-btn").classList.add("hidden");
-            playPause.querySelector(".play-btn").classList.remove("hidden");
+
         }
     };
     return (
         <div className="container">
+
             <div className="player">
                 <div className="player__controls" style={styleControl1} >
                     <div className="player__btn player__btn--small">
@@ -40,10 +60,10 @@ const Player = (props) => {
                 </div>
                 <h2 className="player__artist">Reminber</h2>
                 <h3 className="player__song">The weekend</h3>
-                <input type="range" value="20" min="0" className="player__level" />
+                <input type="range" value={timeSong} min="0" max={timeLenghtSong} className="player__level" id="time_song" />
                 <div className="audio--duration">
-                    <div className="start">00:01</div>
-                    <div className="end">04:30</div>
+                    <div className="start">0{parseInt(timeSong / 60)}:{parseInt(timeSong % 60)}</div>
+                    <div className="end">0{parseInt(timeLenghtSong / 60)}:{parseInt(timeLenghtSong % 60)}</div>
                 </div>
                 <audio className="player__audio" controls id="audio" >
                     <source src="/assets/media/audio.mp3" type="audio/mpeg" />
@@ -53,8 +73,8 @@ const Player = (props) => {
                         <i className="bi bi-skip-backward"></i>
                     </div>
                     <div className="player__btn player__btn--medium blue">
-                        <i className="bi bi-play play-btn" onClick={() => play}></i>
-                        <i className="bi bi-pause pause-btn hide" onClick={() => play}></i>
+                        <i className="bi bi-play play-btn" onClick={() => playMusic()}></i>
+                        <i className="bi bi-pause pause-btn hide" onClick={() => playMusic()}></i>
                     </div>
                     <div className="player__btn player__btn--medium">
                         <i className="bi bi-skip-forward"></i>
